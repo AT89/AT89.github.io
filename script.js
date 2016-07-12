4283c23c23a1fcd455093677fd60f907035d8664
Anh Truong <anh.truong.07@cnu.edu>
2016-07-12 11:50:25 -0400

    reworked links, added Townboard

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-$("#firstsite")
+$("#bio")
 .on("mouseover", function(e){
-  $("#contentBlog").first().html("First website I made, I leave it on here to contrast how far I've come. Of course goofiness was intended.")
+  $("#contentBlog").first().html("I am a modern renaissance man who believes in being better than who I was yesterday. I enjoy learning, being creative and utilizing my resources to create innovative solutions. ")
+});
+
+$("#github")
+.on("mouseover", function(e){
+  $("#contentBlog").first().html("My Github profile: all my projects and learning exercises repositories from General Assembly and my favorite project forks")
+});
+
+$("#email")
+.on("mouseover", function(e){
+  $("#contentBlog").first().html("Feel free to reach out to me!")
 });
 
 $("#war")
@@ -8,9 +18,9 @@ $("#war")
   $("#contentBlog").first().html("First project at General Assembly, it's the War card game in Javascript but with a surprise :) <br>Full Disclosure: lower audio volume")
 });
 
-$("#github")
+$("#townboard")
 .on("mouseover", function(e){
-  $("#contentBlog").first().html("My Github profile: all my projects and learning exercises repositories from General Assembly and my favorite project forks")
+  $("#contentBlog").first().html("My first deployed Ruby on Rails Crud App, with Heroku hosting. I wanted to create a local messageboard for users to share information. Since the boards are seeded, these boards can be used for anything. Still a work in progress.")
 });
 
 $("#bp2015")
@@ -28,20 +38,13 @@ $("#lit")
   $("#contentBlog").first().html("Soundcloud link. Fast. Upbeat. Belligerent music.")
 });
 
+
 $("ul li")
 .on("mouseout", function(e){
   $("#contentBlog").first().html("Where must we go, we who wander this wasteland, in search of our better selves?<br>-The First History Man")
 })
 
-
-
-
-$("#bio")
-.on("mouseover", function(e){
-  $("#contentBlog").first().html("I am a modern renaissance man who believes in being better than who I was yesterday. I enjoy learning, being creative and utilizing my resources to create innovative solutions.")
-});
-
-$("#bio")
+$("a")
 .on("mouseout", function(e){
   $("#contentBlog").first().html("Where must we go, we who wander this wasteland, in search of our better selves?<br>-The First History Man")
 })
