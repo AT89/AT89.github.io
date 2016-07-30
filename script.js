91b4d7efa5bcbc77d5b6eef1ecc05793b71dfa88
Anh Truong <anh.truong.07@cnu.edu>
2016-07-30 11:39:44 -0400

    rework links

--- script.js
+++ script.js
@@ -20,42 +20,42 @@ $("#email")
 });
 
 $("#war")
-.on("mouseover", function(e){
+.on("click", function(e){
   $("#contentBlog").first().html("First project at General Assembly, it's the War card game in Javascript but with a surprise :) <br>Full Disclosure: lower audio volume")
 });
 
 $("#townboard")
-.on("mouseover", function(e){
+.on("click", function(e){
   $("#contentBlog").first().html("My first deployed Ruby on Rails CRUD App, with Heroku hosting. I wanted to create a local messageboard for users to share information. Since the boards are seeded, these boards can be used for anything. Still a work in progress.")
 });
 
 $("#oneaux")
-.on("mouseover", function(e){
+.on("click", function(e){
   $("#contentBlog").first().html("Single-Page (Angular JS) App that utilizes Spotify API to add songs to playlist (Rails API) and allows users to upvote and downvote songs to queue in a Spotify playlist.")
 });
 
 $("#bp2015")
-.on("mouseover", function(e){
+.on("click", function(e){
   $("#contentBlog").first().html("Download (right click save link as) or stream the recorded live mix from the Barn Party 2015. This mix had about 600+ plays on Soundcloud before it was taken down :(")
 });
 
 $("#chill")
-.on("mouseover", function(e){
+.on("click", function(e){
   $("#contentBlog").first().html("Link to my Chill mix series. Downtempo, relaxing, soothing, intimate, enticing music.")
 });
 
 $("#lit")
-.on("mouseover", function(e){
+.on("click", function(e){
   $("#contentBlog").first().html("Soundcloud link. Fast. Upbeat. Belligerent music.")
 });
 
 
-$("ul li")
-.on("mouseout", function(e){
-  $("#contentBlog").first().html("Where must we go, we who wander this wasteland, in search of our better selves?<br>-The First History Man")
-})
+// $("ul li")
+// .on("mouseout", function(e){
+//   $("#contentBlog").first().html("Where must we go, we who wander this wasteland, in search of our better selves?<br>-The First History Man")
+// })
 
-$("a")
+$(".hero")
 .on("mouseout", function(e){
   $("#contentBlog").first().html("Where must we go, we who wander this wasteland, in search of our better selves?<br>-The First History Man")
 })
