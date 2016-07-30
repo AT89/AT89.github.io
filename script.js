64ad3ef6357a9891f633e8142cd2e3471748255b
Anh Truong <anh.truong.07@cnu.edu>
2016-07-30 13:17:08 -0400

    changes to clicks

--- script.js
+++ script.js
@@ -21,32 +21,38 @@ $("#email")
 
 $("#war")
 .on("click", function(e){
-  $("#contentBlog").first().html("First project at General Assembly, it's the War card game in Javascript but with a surprise :) <br>Full Disclosure: lower audio volume")
+  $("#contentBlog").first().html("First project at General Assembly, it's the War card game in Javascript but with a surprise :)<br> <br>Full Disclosure: lower audio volume <br><br><a class='go' href='http://at89.github.io/WARCARDGAME'>App</a> | <a class='go' href='http://github.com/at89/WARCARDGAME'>Github</a>"
+)
 });
 
 $("#townboard")
 .on("click", function(e){
-  $("#contentBlog").first().html("My first deployed Ruby on Rails CRUD App, with Heroku hosting. I wanted to create a local messageboard for users to share information. Since the boards are seeded, these boards can be used for anything. Still a work in progress.")
+  $("#contentBlog").first().html("<br><br>My first deployed Ruby on Rails CRUD App, with Heroku hosting. I wanted to create a local messageboard for users to share information. Since the boards are seeded, these boards can be used for anything. <br><br>Still a work in progress.<br><br><a class='go' href='https://anh-townboard.herokuapp.com/boards/'>App</a> | <a class='go' href='http://github.com/at89/townboard'>Github</a>")
 });
 
 $("#oneaux")
 .on("click", function(e){
-  $("#contentBlog").first().html("Single-Page (Angular JS) App that utilizes Spotify API to add songs to playlist (Rails API) and allows users to upvote and downvote songs to queue in a Spotify playlist.")
+  $("#contentBlog").first().html("<br><br>Single-Page (Angular JS) App that utilizes Spotify API to add songs to playlist (Rails API) and allows users to upvote and downvote songs to queue in a Spotify playlist.<br><br> Still a work in progress. <br>(links broke since we renamed..)<br><br><a class='go' href='https://at89.github.io/oneaux/'>App</a> | <a class='go' href='http://github.com/at89/oneaux'>Github</a>")
 });
 
+// $("#cr2016")
+// .on("mouseover", function(e){
+//   $("#contentBlog").first().html("My mix for Camp Rise 2016 .<br>To be released<br>Not yet recorded/played.")
+// });
+
 $("#bp2015")
-.on("click", function(e){
-  $("#contentBlog").first().html("Download (right click save link as) or stream the recorded live mix from the Barn Party 2015. This mix had about 600+ plays on Soundcloud before it was taken down :(")
+.on("mouseover", function(e){
+  $("#contentBlog").first().html("Download (right click save link as) or stream the recorded live mix from the Barn Party 2015. This mix had about 600+ plays on Soundcloud before it was taken down :(<br><br> <a class='go' href='http://mp3.anhtice.com/Others/BP2015.mp3'>BP 2015</a>")
 });
 
 $("#chill")
-.on("click", function(e){
-  $("#contentBlog").first().html("Link to my Chill mix series. Downtempo, relaxing, soothing, intimate, enticing music.")
+.on("mouseover", function(e){
+  $("#contentBlog").first().html("Link to my Chill mix series. Downtempo, relaxing, soothing, intimate, enticing music.<br><br><a class='go' href='http://anhtice.net23.net'>Listen</a> | <a class='go' href='http://mp3.anhtice.com/Chill/'>Download</a>")
 });
 
 $("#lit")
-.on("click", function(e){
-  $("#contentBlog").first().html("Soundcloud link. Fast. Upbeat. Belligerent music.")
+.on("mouseover", function(e){
+  $("#contentBlog").first().html("Soundcloud link. Fast. Upbeat. Belligerent music.<br><br><a class='go' href='https://soundcloud.com/anhtice/tracks'>SC Link</a>")
 });
 
 
