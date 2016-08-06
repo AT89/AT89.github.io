e4a9f88033650db78d7aeacb5d5036ea208359e7
Anh Truong <anh.truong.07@cnu.edu>
2016-08-06 18:08:21 -0400

    some edits, mobile friendly now

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $("#skills")
 
 $("#skills")
 .on("click", function(e){
-  $("#contentBlog").first().html("<br><br><br><br>Front-end: <br> JQuery, AngularJS, ReactJS <br><br> Back-end:<br> AJAX, SQL, Rails, Express, Mongoose/MongoDB, PostGres, ActiveRecord, Firebase <br><br> Languages:<br> Javascript, Ruby, Python, ELM, PHP <br><br>Misc: <br>CLI, Git, CSS, SASS, TDD (Rspec, Jasmine), Heroku, Wordpress, ES6")
+  $("#contentBlog").first().html("Front-end: <br> JQuery, AngularJS, ReactJS <br><br> Back-end:<br> AJAX, SQL, Rails, Express, Mongoose/MongoDB, PostGres, ActiveRecord, Firebase <br><br> Languages:<br> Javascript, Ruby, Python, ELM, PHP <br><br>Misc: <br>CLI, Git, CSS, SASS, TDD (Rspec, Jasmine), Heroku, Wordpress, ES6")
 });
 
 $("#github")
@@ -36,17 +36,17 @@ $("#war")
 
 $("#townboard")
 .on("click", function(e){
-  $("#contentBlog").first().html("<br><br>My first deployed Ruby on Rails CRUD App, with Heroku hosting. I wanted to create a local messageboard for users to share information. Since the boards are seeded, these boards can be used for anything. <br><br>Still a work in progress.<br><br><a class='go' href='https://anh-townboard.herokuapp.com/boards/'>App</a> | <a class='go2' href='http://github.com/at89/townboard'>Github</a>")
+  $("#contentBlog").first().html("My first deployed Ruby on Rails CRUD App, with Heroku hosting. I wanted to create a local messageboard for users to share information. Since the boards are seeded, these boards can be used for anything. <br><br>Still a work in progress.<br><br><a class='go' href='https://anh-townboard.herokuapp.com/boards/'>App</a> | <a class='go2' href='http://github.com/at89/townboard'>Github</a>")
 });
 
 $("#instagram")
 .on("click", function(e){
-  $("#contentBlog").first().html("<br><br>Instagram Gallery feed.<br> Using slick.js (the best carousel CDN) <br><br><a class='go' href='https://at89.github.io/instagramfeed'>App</a> | <a class='go' href='http://kenwheeler.github.io/slick/'>Slick</a> | <a class='go2' href='https://github.com/AT89/instagramfeed'>Github</a> ")
+  $("#contentBlog").first().html("Instagram Gallery feed.<br> Using slick.js (the best carousel CDN) <br><br><a class='go' href='https://at89.github.io/instagramfeed'>App</a> | <a class='go' href='http://kenwheeler.github.io/slick/'>Slick</a> | <a class='go2' href='https://github.com/AT89/instagramfeed'>Github</a> ")
 });
 
 $("#oneaux")
 .on("click", function(e){
-  $("#contentBlog").first().html("<br><br>Single-Page (Angular JS) App that utilizes Spotify API to add songs to playlist (Rails API) and allows users to upvote and downvote songs to queue in a Spotify playlist.<br><br> Still a work in progress. <br><br><br><a class='go' href='http://oneaux.net/'>App</a> | <a class='go2' href='http://github.com/at89/oneaux'>Github</a>")
+  $("#contentBlog").first().html("Single-Page (Angular JS) App that utilizes Spotify API to add songs to playlist (Rails API) and allows users to upvote and downvote songs to queue in a Spotify playlist.<br><br> Still a work in progress. <br><br><br><a class='go' href='http://oneaux.net/'>App</a> | <a class='go2' href='http://github.com/at89/oneaux'>Github</a>")
 });
 
 // $("#cr2016")
@@ -76,7 +76,7 @@ $("#bio")
 })
 
 
-$("#connect")
-.on("mouseout", function(e){
+$("#github, #linkedin, #email")
+.on("mouseleave", function(e){
   $("#contentBlog").first().html("Where must we go, we who wander this wasteland, in search of our better selves?<br>-The First History Man")
 })
