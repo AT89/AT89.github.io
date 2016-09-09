e69c06c3783b40ff7a5c0bf9b53c32b0ed2a9d9d
Anh Truong <anh.truong.07@cnu.edu>
2016-09-08 21:09:58 -0400

    edited music player

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ Carousel.prototype.go_to = function( index, currentIndex )
 			index = index + this.count;
 
 		this.$.carousel.css({
-			"background-image": "url(pics/MixCovers/chill" + (8-index) + ".jpg)"
+			"background-image": "linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 1)), url(pics/MixCovers/chill" + (8-index) + ".jpg)"
 		});
 
 		/* Make visible or invisible the speakers*/
